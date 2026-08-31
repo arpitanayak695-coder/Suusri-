e2c55aed51ce2f870909fdbd1f9a2bc5fe979aae
arpitanayak695-coder <arpitanayak695@gmail.com>
2026-08-31 23:02:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const mongoose = require('mongoose');
 const localURI = 'mongodb://127.0.0.1:27017/suusriAi';
 
 mongoose.connect(localURI)
-  .then(() => console.log('✅ MongoDB Local Database Connected Successfully!'))
-  .catch((err) => console.error('❌ Connection Error:', err));
+  .then(() => console.log('MongoDB Local Database Connected Successfully!'))
+  .catch((err) => console.error('Connection Error:', err));
